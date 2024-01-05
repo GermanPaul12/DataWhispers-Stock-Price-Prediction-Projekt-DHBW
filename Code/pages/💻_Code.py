f63136712d53ba269a6fc6b703594c6fcc1c5d7a
GermanPaul12 <german.paul@outlook.de>
2024-01-05 14:11:21 +0100

    update code page wiht more code

--- Code/pages/💻_Code.py
+++ Code/pages/💻_Code.py
@@ -7,4 +7,19 @@ if st.checkbox("Web-Scraping with Selenium"):
 
 if st.checkbox("Requirements"):
     st.code(open("Code/requirements.txt").read())   
+
+if st.checkbox("Streamlit"):
+    if st.checkbox("Home"):
+        st.code(open("Code/🏠_Home.py").read())    
+    if st.checkbox("Code"):
+        st.code(open("Code/pages/💻_Code.py").read())
+    if st.checkbox("Premium Customers"):
+        st.code(open("Code/pages/📈_Premium_Customers.py").read())     
+    if st.checkbox("Conception and Diagrams"):
+        st.code(open("Code/pages/📋_Conception_and_Diagrams.py").read())     
+    if st.checkbox("Contact Us"):
+        st.code(open("Code/pages/📝_Contact_Us.py").read())   
+    if st.checkbox("Our Products"):
+        st.code(open("Code/pages/🚀_Our_Products.py").read())      
+         
      
