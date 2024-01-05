054a2284af68dee9686704be99d2fcd1676cdbd9
GermanPaul12 <german.paul@ilcoud.com>
2024-01-05 17:01:47 +0100

    changes in streamlit pages

--- Code/pages/📈_Premium_Customers.py
+++ Code/pages/📈_Premium_Customers.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
 # Log in 
-
+# Initialization
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+if not st.session_state.logged_in:
+    pw = st.text_input("Password", type="password")
+    log_in_button = st.button("Log in")
+if pw == "1234" and log_in_button:
+    st.info("You are logged in!")
+    st.session_state.logged_in = True
+    st.empty()
+elif log_in_button:
+    st.warning("Wrong Password!")
 # Wenn nicht registirert, dann contact us weiterleitung
--- Code/pages/📝_Contact_us.py
+++ Code/pages/📝_Contact_us.py
@@ -1,3 +1,20 @@
 import streamlit as st
+import yagmail
+from keyring import get_keyring
+get_keyring()
 
 # Forms um uns zu kontaktieren.
+with st.form("contact_form", clear_on_submit=True):
+    st.write("Please fill out the form below:")
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    message = st.text_area("Message")
+
+    submit_button = st.form_submit_button("Submit")
+
+    if submit_button:
+        st.write("Thanks for your message!")
+        # Here you can add code to handle the form data
+        # For example, sending an email or storing it in a database
+        yag = yagmail.SMTP('automatedbygerman@gmail.com', st.secrets.app_pw)
+        
