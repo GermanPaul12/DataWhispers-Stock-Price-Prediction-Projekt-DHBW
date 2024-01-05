9599307aab3f3b90e3ecd1991c0e5912f1bb1277
Greif, Michael  SH/HZA-HAADD <greifmch@schaeffler.com>
2024-01-05 13:31:18 +0100

    New Diagramms added

--- Code/pages/📋_Conception_and_Diagramms.py
+++ Code/pages/📋_Conception_and_Diagramms.py
@@ -3,5 +3,8 @@ import streamlit as st
 st.title("Konzeptionelle Modelle und Zeichnungen", anchor='center')
 
 with st.container():  
-    st.image('./WI-Modelle_Bilder_Zeichnungen/Ereignisliste.png')
-    st.image('./')
+    st.image('./WI-Modelle_Bilder_Zeichnungen/Ereignisliste.png', caption='Ereignisliste')
+    st.image('./WI-Modelle_Bilder_Zeichnungen/Ereignis-Reaktions-Modelle.png', caption='Ereignis-Reaktions-Modelle')
+    st.image('./WI-Modelle_Bilder_Zeichnungen/Kontextdiagramm.png', caption='Kontextdiagramm')
+    st.image('./WI-Modelle_Bilder_Zeichnungen/SWOT.png', caption='SWOT-Analyse')
+    st.image('./WI-Modelle_Bilder_Zeichnungen/Kombinierte_SWOT_Analyse.png', caption='Kombinierte SWOT-Analyse')
