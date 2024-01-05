64d593dd61928b06dbe043219c44b158829c065f
Greif, Michael  SH/HZA-HAADD <greifmch@schaeffler.com>
2024-01-05 13:27:12 +0100

    Conception and Art as Webpage added

---
+++ Code/pages/📋_Conception_and_Diagramms.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Konzeptionelle Modelle und Zeichnungen", anchor='center')
+
+with st.container():  
+    st.image('./WI-Modelle_Bilder_Zeichnungen/Ereignisliste.png')
+    st.image('./')
