289c4aaa2ce904a7ca18d02e1ad0173339d14a56
Greif, Michael  SH/HZA-HAADD <greifmch@schaeffler.com>
2024-01-05 14:18:48 +0100

    Changed Diagrams to dark mode

--- Code/pages/📋_Conception_and_Diagrams.py
+++ Code/pages/📋_Conception_and_Diagrams.py
@@ -5,12 +5,12 @@ with st.container():
     st.write("Um den Erstellungsprozess dieser Webapplikation genausmöglich dokumentieren zu können, sind hier die verscheidnen benutzten Modelle zu Projektentwicklung angegeben, die im folgenden spezifisch erklährt werden.")
 
 with st.container():  
-    st.image('WI-Modelle_Bilder_Zeichnungen/Ereignisliste.png', caption='Ereignisliste')
+    st.image('WI-Modelle_Bilder_Zeichnungen/Ereignisliste_dark.png', caption='Ereignisliste')
     st.write("\n")
-    st.image('WI-Modelle_Bilder_Zeichnungen/Ereignis-Reaktions-Modelle.png', caption='Ereignis-Reaktions-Modelle')
+    st.image('WI-Modelle_Bilder_Zeichnungen/Ereignis-Reaktions-Modelle-dark.png', caption='Ereignis-Reaktions-Modelle')
     st.write("\n")
-    st.image('WI-Modelle_Bilder_Zeichnungen/Kontextdiagramm.png', caption='Kontextdiagramm')
+    st.image('WI-Modelle_Bilder_Zeichnungen/Kontextdiagramm_dark.png', caption='Kontextdiagramm')
     st.write("\n")
-    st.image('WI-Modelle_Bilder_Zeichnungen/SWOT.png', caption='SWOT-Analyse')
+    st.image('WI-Modelle_Bilder_Zeichnungen/SWOT_dark.png', caption='SWOT-Analyse')
     st.write("\n")
-    st.image('WI-Modelle_Bilder_Zeichnungen/Kombinierte_SWOT_Analyse.png', caption='Kombinierte SWOT-Analyse')
+    st.image('WI-Modelle_Bilder_Zeichnungen/Kombinierte_SWOT_Analyse_dark.png', caption='Kombinierte SWOT-Analyse')
