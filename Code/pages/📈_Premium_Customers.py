a3b9c4c37fe57e0cf8430667ba5211143e7134f4
GermanPaul12 <german.paul@ilcoud.com>
2024-01-05 17:21:22 +0100

    update premium cusotmers

--- Code/pages/📈_Premium_Customers.py
+++ Code/pages/📈_Premium_Customers.py
@@ -2,11 +2,8 @@ import streamlit as st
 
 # Log in 
 # Initialization
-if 'logged_in' not in st.session_state:
-    st.session_state['logged_in'] = False
-if not st.session_state.logged_in:
-    pw = st.text_input("Password", type="password")
-    log_in_button = st.button("Log in")
+pw = st.text_input("Password", type="password")
+log_in_button = st.button("Log in")
 if pw == "1234" and log_in_button:
     st.info("You are logged in!")
     st.session_state.logged_in = True
