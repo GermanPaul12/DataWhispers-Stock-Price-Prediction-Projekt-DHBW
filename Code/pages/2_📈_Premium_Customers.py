7a5d24a85900df00e4efe1b35c15c81ecf3bd17f
GermanPaul12 <german.paul@outlook.de>
2024-01-10 21:02:35 +0100

    insert container in premium customers

--- Code/pages/2_📈_Premium_Customers.py
+++ Code/pages/2_📈_Premium_Customers.py
@@ -8,6 +8,8 @@ import cufflinks as cf
 
 st.set_page_config(page_title='Premium Customers',page_icon='💹')
 st.title("Premium Customer Dashboard")
+
+
 def get_stock_data(interval="1mo", symbol="AMRN", range_="5y", region="US"):
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-chart"
 
@@ -44,25 +46,27 @@ def get_stock_symbol(querystring="APPLE", region="US"):
 #log_in_button = st.button("Log in")
 #if pw == "1234" and log_in_button:
 #st.info("You are logged in!")
-st.session_state.logged_in = True
-st.empty()
-col1,col2 = st.columns(2)
-with col1: 
-    query_stock_string = st.text_input("Enter a company name")
-    query_stock_region = st.selectbox("Select a region", ["US", "BR", "AU", "CA", "FR", "DE", "HK", "IN", "ES", "GB", "SG"])
-with col2: 
-    st.text('')
-    st.text('')
-    st.text('')
-    st.text('')
-    button_to_get_stock_symbol = st.button("Get stock symbol")  
-    if button_to_get_stock_symbol:
-        json_file_stock_symbol = get_stock_symbol(query_stock_string, query_stock_region)
-        st.json(json_file_stock_symbol)
-        content = json.dumps(json_file_stock_symbol)
-        df = pd.read_json(content, orient='index')
-        df = df.transpose()    
-        df.to_csv("Code/data/stock_symbols.csv")
+with st.container(border="1px solid black"):
+    st.session_state.logged_in = True
+    st.empty()
+    col1,col2 = st.columns(2)
+    with col1: 
+        query_stock_string = st.text_input("Enter a company name")
+        query_stock_region = st.selectbox("Select a region", ["US", "BR", "AU", "CA", "FR", "DE", "HK", "IN", "ES", "GB", "SG"])
+    with col2: 
+        st.text('')
+        st.text('')
+        st.text('')
+        st.text('')
+        button_to_get_stock_symbol = st.button("Get stock symbol")  
+        if button_to_get_stock_symbol:
+            json_file_stock_symbol = get_stock_symbol(query_stock_string, query_stock_region)
+            st.json(json_file_stock_symbol)
+            content = json.dumps(json_file_stock_symbol)
+            df = pd.read_json(content, orient='index')
+            df = df.transpose()    
+            df.to_csv("Code/data/stock_symbols.csv")
+           
         
             
     
