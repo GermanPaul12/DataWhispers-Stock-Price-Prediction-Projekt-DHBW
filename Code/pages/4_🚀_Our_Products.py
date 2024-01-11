d044516fd9df4d4409becd708dc0aa27d2be85f4
Greif, Michael  SH/HZA-HAADD <greifmch@schaeffler.com>
2024-01-11 19:16:26 +0100

    local Fix

--- Code/pages/4_🚀_Our_Products.py
+++ Code/pages/4_🚀_Our_Products.py
@@ -4,7 +4,7 @@ import numpy as np
 import plotly.graph_objs as go
 import plotly.express as px
 import locale
-locale.setlocale(locale.LC_ALL, 'de_DE')
+#locale.setlocale(locale.LC_ALL, 'de_DE')
 
 # Asset Allocation Model
 
