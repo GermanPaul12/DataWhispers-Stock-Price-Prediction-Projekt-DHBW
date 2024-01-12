dd5965b6c99d1b4edc0fc411de7c37def4802306
GermanPaul12 <german.paul@icloud.com>
2024-01-12 10:45:27 +0100

    chage y values in prediction plot

--- Code/pages/4_🚀_Our_Products.py
+++ Code/pages/4_🚀_Our_Products.py
@@ -92,7 +92,7 @@ st.title("Our Products 🚀")
 with st.expander("Dow Jones Prediction"):
     df = pd.read_csv(r"Code/data/dow_jones_prediction.csv")
     df = df.rename(columns={"REG_Close": "Regression Title", "DOV_Close": "DOV2VEC", "SENT_TRANS_Close": "Sentence Transformer", "Dow_Jones_Close": "Dow Jones"})
-    fig = px.line(df, x="date", y=['REG_Close', "DOV_Close", "SENT_TRANS_Close", "Dow_Jones_Close"], title='Dow Jones Prediction', color_discrete_sequence=px.colors.sequential.RdBu)
+    fig = px.line(df, x="date", y=['Regression Title', "DOV2VEC", "Sentence Transformer", "Dow Jones"], title='Dow Jones Prediction', color_discrete_sequence=px.colors.sequential.RdBu)
     st.plotly_chart(fig, use_container_width=True)
 
 # Dow Jones Prediction
