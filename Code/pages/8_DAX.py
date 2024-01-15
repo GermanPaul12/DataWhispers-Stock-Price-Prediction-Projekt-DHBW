245cf2ebe29b51cd8aa732053a701ebad7bab824
Greif, Michael  SH/HZA-HAADD <greifmch@schaeffler.com>
2024-01-15 17:24:12 +0100

    Bug with selenium

--- Code/pages/8_DAX.py
+++ Code/pages/8_DAX.py
@@ -2,45 +2,8 @@ import pandas as pd
 import streamlit as st
 import time
 import numpy as np
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import os
 from datetime import datetime, timedelta
-
-# def getDriver():
-#     user_agent = "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html)"  # Google Bot User Agent
-#     user_agent="Code\data\chromedriver.exe"
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--headless')
-#     options.add_argument(f'user-agent={user_agent}')
-#     options.add_argument('--no-sandbox')
-#     options.add_argument('—-disable-extensions')
-#     driver = webdriver.Chrome(options=options)
-    
-#     return driver
-
-# def getDataFrame(Elemente):
-#     Kurse=Elemente.split("\n")[1:-1]
-#     coloumnNames=[x for x in Kurse.pop(0).split()]
-#     coloumnNames.remove("Relation")
-#     dat=[x.strip().split() for x in Kurse]
-#     data=[]
-#     DATEN=[]
-#     for x in range(0,len(dat)-1,2):
-#         data.append(dat[x]+dat[x+1])
-#     for x in data:
-#         DATEN.append([" ".join(x[:-7])] + x[-7:-3] +[x[-3] +" "+ x[-2]] + [x[-1]])
-#     return pd.DataFrame(columns=coloumnNames, data=DATEN)
-
-# def Dax_Scrapping():
-#     url="https://www.tagesschau.de/wirtschaft/boersenkurse/dax-index-846900/"
-
-#     driver=getDriver()
-#     driver.get(url)
-#     element=driver.find_element(By.XPATH,"/html/body")
-#     Kurse = element.text.split("Wertpapiere im DAX ® (40)")[1].split("Die Daten werden")[0]
-#     driver.quit()
-#     return getDataFrame(Kurse)
 
 url="https://www.tagesschau.de/wirtschaft/boersenkurse/dax-index-846900/"
 df = pd.read_html(url)[-1].drop(columns="Relation")
